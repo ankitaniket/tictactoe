eb5c24b2ec434c33cb7fb16ae404d74a137b2e5b
ankitaniket <ankitaniket3@gmail.com>
2022-07-03 21:11:16 +0530

    A new class intent for offline game view

---
+++ TicTacToe/app/src/main/java/com/example/tictactoe/Game_offline.java
@@ -0,0 +1,13 @@
+package com.example.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Game_offline extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_offline);
+    }
+}
