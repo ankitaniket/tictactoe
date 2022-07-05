f10564838ae75f93760747aee50702d899a0aec8
ankitaniket <ankitaniket3@gmail.com>
2022-07-05 20:16:59 +0530

    Adding a new main screen using java in-build object INTENT, two buttons with an image.

--- TicTacToe/app/src/main/java/com/example/tictactoe/Game_offline.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/Game_offline.java
@@ -1,13 +1,129 @@
 package com.example.tictactoe;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Arrays;
 
 public class Game_offline extends AppCompatActivity {
+
+    boolean gameActive = true;
+    // Player representation
+    // 0 - X
+    // 1 - O
+
+    int activePlayer = 0;
+    int[] gameState = {2, 2 , 2, 2, 2, 2, 2, 2, 2};
+    //    State meanings:
+    //    0 - X
+    //    1 - O
+    //    2 - Null
+    int[][] winPositions = {{0,1,2}, {3,4,5}, {6,7,8},
+            {0,3,6}, {1,4,7}, {2,5,8},
+            {0,4,8}, {2,4,6}};
+
+
+
+    public void playerTap(View view){
+        ImageView img = (ImageView) view;
+        int tappedImage = Integer.parseInt(img.getTag().toString());
+        if(!gameActive){
+            gameReset(view);
+        }
+        if(gameState[tappedImage] == 2) {
+            gameState[tappedImage] = activePlayer;
+            if (activePlayer == 0) {
+                img.setImageResource(R.drawable.x);
+                activePlayer = 1;
+                TextView status = findViewById(R.id.status);
+                status.setText(R.string.str2);
+            } else {
+                img.setImageResource(R.drawable.o);
+                activePlayer = 0;
+                TextView status = findViewById(R.id.status);
+                status.setText(R.string.str1);
+            }
+        }
+        // Draw Case
+        boolean emptySquare = false;
+        for (int squareState : gameState) {
+            if (squareState == 2) {
+                emptySquare = true;
+                break;
+            }
+        }
+        if (!emptySquare && gameActive) {
+            // Game is a draw
+            gameActive = false;
+            String winnerStr;
+            winnerStr = "It's a Draw \n Tap to play again";
+            TextView status = findViewById(R.id.status);
+            status.setText(winnerStr);
+
+        }
+
+        // Check if any player has won
+        for(int[] winPosition: winPositions){
+            if(gameState[winPosition[0]] == gameState[winPosition[1]] &&
+                    gameState[winPosition[1]] == gameState[winPosition[2]] &&
+                    gameState[winPosition[0]]!=2){
+                // Somebody has won! - Find out who!
+                String winnerStr;
+                gameActive = false;
+                if(gameState[winPosition[0]] == 0){
+                    winnerStr = "X has won";
+
+                }
+                else{
+                    winnerStr = "O has won";
+                }
+                // Update the status bar for winner announcement
+                TextView status = findViewById(R.id.status);
+                status.setText(winnerStr);
+
+            }
+        }
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void gameReset(View view) {
+        gameActive = true;
+        activePlayer = 0;
+        Arrays.fill(gameState, 2);
+
+        for (int i : new int[]{R.id.imageView0, R.id.imageView1, R.id.imageView2, R.id.imageView3, R.id.imageView4, R.id.imageView5, R.id.imageView6, R.id.imageView7, R.id.imageView8}) {
+            ((ImageView)findViewById(i)).setImageResource(0);
+        }
+
+        TextView status = findViewById(R.id.status);
+        status.setText("X's Turn - Tap to play");
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_offline);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void Reset(View view) {
+        gameActive = true;
+        activePlayer = 0;
+        Arrays.fill(gameState, 2);
+
+        for (int i : new int[]{R.id.imageView0, R.id.imageView1, R.id.imageView2, R.id.imageView3, R.id.imageView4, R.id.imageView5, R.id.imageView6, R.id.imageView7, R.id.imageView8}) {
+            ((ImageView)findViewById(i)).setImageResource(0);
+        }
+
+        TextView status = findViewById(R.id.status);
+        status.setText("X's Turn - Tap to play");
     }
 }
--- TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,126 +1,43 @@
 package com.example.tictactoe;
+
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-import java.util.Arrays;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    boolean gameActive = true;
-    // Player representation
-    // 0 - X
-    // 1 - O
 
-    // hello check
+        // define the global variable
 
-    int activePlayer = 0;
-    int[] gameState = {2, 2 , 2, 2, 2, 2, 2, 2, 2};
-    //    State meanings:
-    //    0 - X
-    //    1 - O
-    //    2 - Null
-    int[][] winPositions = {{0,1,2}, {3,4,5}, {6,7,8},
-            {0,3,6}, {1,4,7}, {2,5,8},
-            {0,4,8}, {2,4,6}};
+        // Add button Move to Activity
 
+        Button next_Activity_button;
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            next_Activity_button = (Button)findViewById(R.id.first_activity_button);
 
 
-    public void playerTap(View view){
-        ImageView img = (ImageView) view;
-        int tappedImage = Integer.parseInt(img.getTag().toString());
-        if(!gameActive){
-            gameReset(view);
-        }
-        if(gameState[tappedImage] == 2) {
-            gameState[tappedImage] = activePlayer;
-            if (activePlayer == 0) {
-                img.setImageResource(R.drawable.x);
-                activePlayer = 1;
-                TextView status = findViewById(R.id.status);
-                status.setText(R.string.str2);
-            } else {
-                img.setImageResource(R.drawable.o);
-                activePlayer = 0;
-                TextView status = findViewById(R.id.status);
-                status.setText(R.string.str1);
-            }
-        }
-        // Draw Case
-        boolean emptySquare = false;
-        for (int squareState : gameState) {
-            if (squareState == 2) {
-                emptySquare = true;
-                break;
-            }
-        }
-        if (!emptySquare && gameActive) {
-            // Game is a draw
-            gameActive = false;
-            String winnerStr;
-            winnerStr = "It's a Draw \n Tap to play again";
-            TextView status = findViewById(R.id.status);
-            status.setText(winnerStr);
-        }
+            // Add_button add clicklistener
+            next_Activity_button.setOnClickListener(new View.OnClickListener() {
 
-        // Check if any player has won
-        for(int[] winPosition: winPositions){
-            if(gameState[winPosition[0]] == gameState[winPosition[1]] &&
-                    gameState[winPosition[1]] == gameState[winPosition[2]] &&
-                    gameState[winPosition[0]]!=2){
-                // Somebody has won! - Find out who!
-                String winnerStr;
-                gameActive = false;
-                if(gameState[winPosition[0]] == 0){
-                    winnerStr = "X has won";
+                public void onClick(View v)
+                {
+                    Intent intent = new Intent(MainActivity.this, Game_offline.class);
+
+                    // start the activity connect to the specified class
+                    startActivity(intent);
                 }
-                else{
-                    winnerStr = "O has won";
-                }
-                // Update the status bar for winner announcement
-                TextView status = findViewById(R.id.status);
-                status.setText(winnerStr);
-
-            }
+            });
         }
-
-    }
-
-    @SuppressLint("SetTextI18n")
-    public void gameReset(View view) {
-        gameActive = true;
-        activePlayer = 0;
-        Arrays.fill(gameState, 2);
-
-        for (int i : new int[]{R.id.imageView0, R.id.imageView1, R.id.imageView2, R.id.imageView3, R.id.imageView4, R.id.imageView5, R.id.imageView6, R.id.imageView7, R.id.imageView8}) {
-            ((ImageView)findViewById(i)).setImageResource(0);
-        }
-
-        TextView status = findViewById(R.id.status);
-        status.setText("X's Turn - Tap to play");
-
     }
 
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @SuppressLint("SetTextI18n")
-    public void Reset(View view) {
-        gameActive = true;
-        activePlayer = 0;
-        Arrays.fill(gameState, 2);
-
-        for (int i : new int[]{R.id.imageView0, R.id.imageView1, R.id.imageView2, R.id.imageView3, R.id.imageView4, R.id.imageView5, R.id.imageView6, R.id.imageView7, R.id.imageView8}) {
-            ((ImageView)findViewById(i)).setImageResource(0);
-        }
-
-        TextView status = findViewById(R.id.status);
-        status.setText("X's Turn - Tap to play");
-    }
-}
