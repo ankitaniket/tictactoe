ad66449c6b3cc3e5b7287bf5bdae2a39627106f6
ankitaniket <ankitaniket3@gmail.com>
2022-07-09 14:50:05 +0530

    Toast message while click on "WITH AI" button

--- TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.tictactoe;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,25 +13,33 @@ public class MainActivity extends AppCompatActivity {
 
         // Add button Move to Activity
 
-        Button next_Activity_button;
+        Button offline_button;
+        Button ai_button;
 
         protected void onCreate(Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
-            next_Activity_button = findViewById(R.id.bt1);
+            offline_button = findViewById(R.id.bt1);
+            ai_button = findViewById(R.id.bt2);
+
+
 
 
             // Add_button add clicklistener
-            next_Activity_button.setOnClickListener(v -> {
+            offline_button.setOnClickListener(v -> {
                 Intent intent = new Intent(MainActivity.this, Game_offline.class);
 
                 // start the activity connect to the specified class
                 startActivity(intent);
             });
+
+            ai_button.setOnClickListener(view -> Toast.makeText(getApplicationContext(), "There is no activity",
+                    Toast.LENGTH_LONG).show());
         }
-    }
+
+}
 
 
 
