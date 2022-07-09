222d4072ed9e0c4644b608ccc23717f579d07e85
ankitaniket <ankitaniket3@gmail.com>
2022-07-09 14:13:10 +0530

    fix frontend

--- TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.tictactoe;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,26 +13,20 @@ public class MainActivity extends AppCompatActivity {
 
         Button next_Activity_button;
 
-        @SuppressLint("SetTextI18n")
-        @Override
         protected void onCreate(Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
-            next_Activity_button = (Button)findViewById(R.id.first_activity_button);
+            next_Activity_button = findViewById(R.id.bt1);
 
 
             // Add_button add clicklistener
-            next_Activity_button.setOnClickListener(new View.OnClickListener() {
+            next_Activity_button.setOnClickListener(v -> {
+                Intent intent = new Intent(MainActivity.this, Game_offline.class);
 
-                public void onClick(View v)
-                {
-                    Intent intent = new Intent(MainActivity.this, Game_offline.class);
-
-                    // start the activity connect to the specified class
-                    startActivity(intent);
-                }
+                // start the activity connect to the specified class
+                startActivity(intent);
             });
         }
     }
